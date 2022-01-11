97afe4546600f4ba973c5d10ae618028fcccaf70
Dante Carrasco <arielcarp1996@gmail.com>
2022-01-10 22:55:48 -0300

    Adding infoWindow in google map api and modifying Map component structure

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,22 +1,26 @@
-import { useLoadScript, GoogleMap, Marker } from "@react-google-maps/api"
+import { useLoadScript, GoogleMap, Marker, InfoWindow } from "@react-google-maps/api"
 import { useRef, useCallback, useState, useEffect } from "react"
 
 const Map = () => {
     const googleApiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY
     const latCoordinate = -34.71437824101841
     const lngCoordinate = -58.535330158467374
-    const [ userMarker, setUserMarker ] = useState([])
+    const myCoordinates = {
+        id: 1,
+        lat: latCoordinate,
+        lng: lngCoordinate
+    }
+    const [ coordinates, setCoordinates ] = useState([myCoordinates])
+    const [ markerSelected, setMarkerSelected ] = useState(null)
     const mapRef = useRef()
-    const onMapLoad = useCallback((map) => {
-        mapRef.current = map
-    }, [])
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(
             (position) => {
-                setUserMarker(current => 
+                setCoordinates(current => 
                     [
                         ...current,
                         {
+                            id: 2,
                             lat: position.coords.latitude,
                             lng: position.coords.longitude
                         }
@@ -24,11 +28,21 @@ const Map = () => {
             },
             () => null
         )
-
-        return (() => {
-            setUserMarker([])
-        })
     }, [])
+
+    const onMapLoad = useCallback((map) => {
+        const bounds = new google.maps.LatLngBounds()
+        coordinates.forEach(coordinate => {
+            bounds.extend(
+                new google.maps.LatLng(
+                    coordinate.lat,
+                    coordinate.lng
+                )
+            )
+        })
+        mapRef.current = map
+        map.fitBounds(bounds)
+    }, [ coordinates ])
 
     const { isLoaded, loadError } = useLoadScript({
         googleMapsApiKey: googleApiKey
@@ -45,30 +59,43 @@ const Map = () => {
             left: 0,
             width: "100%",
             height: "100%"
-        }}
-        zoom={15}
-        center={{
-            lat: latCoordinate, 
-            lng: lngCoordinate
-        }}
+        }} 
         options={{
             disableDefaultUI: true,
             zoomControl: true
         }}
         onLoad={onMapLoad}>
-            <Marker 
-            position={{ 
-                lat: latCoordinate,
-                lng: lngCoordinate}} />
-            {userMarker.length > 0 &&
-                userMarker.map((marker, index) => 
-                    <Marker
-                    key={index + 1}
-                    position={{
-                        lat: marker.lat,
-                        lng: marker.lng
-                    }} />
-                )
+            {coordinates.map((marker, index) => 
+                <Marker
+                key={index + 1}
+                position={{
+                    lat: marker.lat,
+                    lng: marker.lng
+                }} 
+                onClick={() => {
+                    setMarkerSelected(marker)
+                }} />
+            )}
+            {markerSelected &&
+                <InfoWindow
+                position={{
+                    lat: markerSelected.lat,
+                    lng: markerSelected.lng
+                }}
+                onCloseClick={() => {
+                    setMarkerSelected(null)
+                }}>
+                    <div className="google-info">
+                        {markerSelected.id === 1
+                            ? (
+                                <h3>¡Mi ubicación!</h3>
+                            )
+                            : (
+                                <h3>¡Tu ubicación!</h3>
+                            )
+                        }
+                    </div>
+                </InfoWindow>
             }
         </GoogleMap>
     )
