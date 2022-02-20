be8cf48d8deb86861c4232d3be81bd52b9f47d1d
Dante Carrasco <arielcarp1996@gmail.com>
2022-02-20 12:35:44 -0300

    name changes without importance

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,8 @@
-import MainBanner from "../templates/MainBanner"
+import MainBanner from "../templates/Banner"
 import AboutMe from "../templates/AboutMe"
-import Education from "../templates/education"
-import ContactInfo from "../templates/ContactInfo"
-import Skills from "../templates/skills"
+import Education from "../templates/educationn"
+import ContactInfo from "../templates/Contact"
+import Skills from "../templates/skillss"
 import { useEffect } from "react"
 
 const Home = () => {
