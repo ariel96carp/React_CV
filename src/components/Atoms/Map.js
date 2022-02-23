fd97c29a3f10ff292091385f689d281ddaf20272
Dante Carrasco <arielcarp1996@gmail.com>
2022-02-22 23:48:06 -0300

    deleting unused importations in map component and optimizing it

--- src/components/Atoms/Map.js
+++ src/components/Atoms/Map.js
@@ -1,4 +1,4 @@
-import { useLoadScript, GoogleMap, Marker, InfoWindow } from "@react-google-maps/api"
+import { useLoadScript, GoogleMap, Marker } from "@react-google-maps/api"
 import { useRef, useCallback, useState, useEffect } from "react"
 
 const Map = () => {
@@ -6,12 +6,10 @@ const Map = () => {
     const latCoordinate = -34.71437824101841
     const lngCoordinate = -58.535330158467374
     const myCoordinates = {
-        id: 1,
         lat: latCoordinate,
         lng: lngCoordinate
     }
     const [ coordinates, setCoordinates ] = useState([myCoordinates])
-    const [ markerSelected, setMarkerSelected ] = useState(null)
     const mapRef = useRef()
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(
@@ -20,7 +18,6 @@ const Map = () => {
                     [
                         ...current,
                         {
-                            id: 2,
                             lat: position.coords.latitude,
                             lng: position.coords.longitude
                         }
@@ -71,32 +68,8 @@ const Map = () => {
                 position={{
                     lat: marker.lat,
                     lng: marker.lng
-                }} 
-                onClick={() => {
-                    setMarkerSelected(marker)
                 }} />
             )}
-            {markerSelected &&
-                <InfoWindow
-                position={{
-                    lat: markerSelected.lat,
-                    lng: markerSelected.lng
-                }}
-                onCloseClick={() => {
-                    setMarkerSelected(null)
-                }}>
-                    <div className="google-info">
-                        {markerSelected.id === 1
-                            ? (
-                                <h3>¡Mi ubicación!</h3>
-                            )
-                            : (
-                                <h3>¡Tu ubicación!</h3>
-                            )
-                        }
-                    </div>
-                </InfoWindow>
-            }
         </GoogleMap>
     )
 }
